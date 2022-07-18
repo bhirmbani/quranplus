cb5c12868c96aacc1079a6f0778ab7899c983d98
Bhirawa Mbani <bhirmbani@gmail.com>
2022-07-18 20:27:38 +0700

    init play pause audio

--- app/components/surah-options-card.tsx
+++ app/components/surah-options-card.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { generateVerseNumber } from "~/utils/data-manipulation";
 
 type SurahOptionsCardType = {
@@ -6,39 +7,71 @@ type SurahOptionsCardType = {
 };
 
 const SurahOptionsCard = ({ surahIdx, verseIdx }: SurahOptionsCardType) => {
-  const handlePlayButton = async () => {
+  const [paused, setPaused] = useState(true);
+  const [currentTime, setCurrentTime] = useState(0);
+  const handlePlayPauseButton = async () => {
+    const audio = document.getElementById("verse-audio") as HTMLMediaElement;
     const number = generateVerseNumber(surahIdx, verseIdx);
-    const music = new Audio(
-      `https://cdn.islamic.network/quran/audio/128/ar.alafasy/${number}.mp3`
-    );
-    await music.play();
+    if (paused) {
+      audio.src = `https://cdn.islamic.network/quran/audio/128/ar.alafasy/${number}.mp3`;
+      audio.currentTime = audio.ended ? 0 : currentTime;
+      audio.play();
+      setPaused(false);
+      setCurrentTime(audio.currentTime + 1);
+    } else {
+      const time = audio.ended ? 0 : audio.currentTime;
+      setCurrentTime(time);
+      audio.pause();
+      setPaused(true);
+    }
   };
 
   return (
-    <div className="mx-5 flex flex-row justify-around mb-5 card-bordered card card-body p-0">
-      <button onClick={handlePlayButton} className="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+    <div className="mx-5 flex flex-row justify-around mb-5 card-bordered card card-body p-2">
+      <audio id="verse-audio" />
+      <button
+        onClick={handlePlayPauseButton}
+        className="btn btn-xs btn-ghost btn-circle"
+      >
+        {!paused ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        )}
       </button>
       {/* bookmark */}
-      <button className="btn btn-ghost btn-circle">
+      <button className="btn btn-xs btn-ghost btn-circle">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -55,7 +88,7 @@ const SurahOptionsCard = ({ surahIdx, verseIdx }: SurahOptionsCardType) => {
         </svg>
       </button>
       {/* check */}
-      <button className="btn btn-ghost btn-circle">
+      <button className="btn btn-xs btn-ghost btn-circle">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
@@ -72,7 +105,7 @@ const SurahOptionsCard = ({ surahIdx, verseIdx }: SurahOptionsCardType) => {
         </svg>
       </button>
       {/* archive */}
-      <button className="btn btn-ghost btn-circle">
+      <button className="btn btn-xs btn-ghost btn-circle">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
