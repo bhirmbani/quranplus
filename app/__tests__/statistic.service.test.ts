6d8c3295ab23b8484995e30aa3e1d47b0f4e0745
Bhirawa Mbani <bhirmbani@gmail.com>
2022-08-08 21:27:29 +0700

    update statistic testing

--- app/__tests__/statistic.service.test.ts
+++ app/__tests__/statistic.service.test.ts
@@ -2,11 +2,12 @@ import db from "~/models";
 import { updateStatistics } from "~/services/stats";
 import {
   createdDate,
+  memorizedOneSurahWithDifferentDate,
   memorizeOneSurahExpectedResult,
   memorizeOneVerseExpectedResult,
+  memorizeOneVerseWithDifferentDate,
   unmemorizeOneVerseExpectedResult,
 } from "~/__mocks__/statistic.service.expected";
-import { formatDateInProgress } from "~/utils/test-helpers";
 import { inititalStateMemorized } from "~/__mocks__/statistic.service.mockdata";
 
 // const constantDate = new Date("2018-01-01T12:00:00");
@@ -17,17 +18,20 @@ import { inititalStateMemorized } from "~/__mocks__/statistic.service.mockdata";
 //   }
 // };
 
-beforeAll(() => {
-  jest.useFakeTimers();
-  jest.setSystemTime(new Date(createdDate));
-});
-
 afterEach(() => {
   db.statistic.clear();
+  jest.useFakeTimers();
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
 });
 
 // MEMORIZED
 describe("memorize a verse", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(createdDate);
+  });
+
   test("should increment verse by one", async () => {
     const memorizeVerseResult = await updateStatistics("memorized", {
       type: "verse",
@@ -36,18 +40,14 @@ describe("memorize a verse", () => {
       verseIdx: 0,
     });
 
-    expect({
-      ...memorizeVerseResult,
-      progress: formatDateInProgress(memorizeVerseResult!.progress),
-    }).toMatchObject({
-      ...memorizeOneVerseExpectedResult,
-      progress: formatDateInProgress(memorizeOneVerseExpectedResult!.progress),
-    });
+    expect(memorizeVerseResult).toMatchObject(memorizeOneVerseExpectedResult);
   });
 });
 
 describe("unmemorize a verse", () => {
   beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(createdDate);
     db.statistic.add({
       last_read: {
         verse_idx: 0,
@@ -68,19 +68,17 @@ describe("unmemorize a verse", () => {
       verseIdx: 0,
     });
 
-    expect({
-      ...unmemorizeVerseResult,
-      progress: formatDateInProgress(unmemorizeVerseResult!.progress),
-    }).toMatchObject({
-      ...unmemorizeOneVerseExpectedResult,
-      progress: formatDateInProgress(
-        unmemorizeOneVerseExpectedResult!.progress
-      ),
-    });
+    expect(unmemorizeVerseResult).toMatchObject(
+      unmemorizeOneVerseExpectedResult
+    );
   });
 });
 
 describe("memorize a surah", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(createdDate);
+  });
   test("should increment surah by one and verse by verse length", async () => {
     const memorizeSurahResult = await updateStatistics("memorized", {
       type: "surah",
@@ -89,18 +87,14 @@ describe("memorize a surah", () => {
       verseIdx: 0,
     });
 
-    expect({
-      ...memorizeSurahResult,
-      progress: formatDateInProgress(memorizeSurahResult!.progress),
-    }).toMatchObject({
-      ...memorizeOneSurahExpectedResult,
-      progress: formatDateInProgress(memorizeOneSurahExpectedResult!.progress),
-    });
+    expect(memorizeSurahResult).toMatchObject(memorizeOneSurahExpectedResult);
   });
 });
 
 describe("unmemorize a surah", () => {
   beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(createdDate);
     db.statistic.add({
       last_read: {
         verse_idx: 0,
@@ -121,12 +115,66 @@ describe("unmemorize a surah", () => {
       verseIdx: 0,
     });
 
-    expect({
-      ...unmemorizeSurahResult,
-      progress: formatDateInProgress(unmemorizeSurahResult!.progress),
-    }).toMatchObject({
-      ...inititalStateMemorized,
-      progress: formatDateInProgress(inititalStateMemorized!.progress),
+    expect(unmemorizeSurahResult).toMatchObject(inititalStateMemorized);
+  });
+});
+
+describe("memorize a verse with different date", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date("2022-08-08T11:29:45.549Z"));
+    db.statistic.add({
+      last_read: {
+        verse_idx: 0,
+        surah_idx: 0,
+      },
+      daily_checkin: {
+        last_date: new Date(),
+        streak: 0,
+      },
+      memorized: memorizeOneSurahExpectedResult,
     });
+  });
+  test("should add new date in progress and increment verse by one", async () => {
+    const memorizeVerseResult = await updateStatistics("memorized", {
+      type: "verse",
+      memorize: true,
+      surahIdx: 113,
+      verseIdx: 0,
+    });
+
+    expect(memorizeVerseResult).toMatchObject(
+      memorizeOneVerseWithDifferentDate
+    );
+  });
+});
+
+describe("memorize a surah with different date", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date("2022-08-08T11:29:45.549Z"));
+    db.statistic.add({
+      last_read: {
+        verse_idx: 0,
+        surah_idx: 0,
+      },
+      daily_checkin: {
+        last_date: new Date(),
+        streak: 0,
+      },
+      memorized: memorizeOneSurahExpectedResult,
+    });
+  });
+  test("should add new date in progress and increment surah by one and verse by surah's verse length", async () => {
+    const surahIdx = 113;
+    const memorizeSurahResult = await updateStatistics("memorized", {
+      type: "surah",
+      memorize: true,
+      surahIdx,
+    });
+
+    expect(memorizeSurahResult).toMatchObject(
+      memorizedOneSurahWithDifferentDate
+    );
   });
 });
